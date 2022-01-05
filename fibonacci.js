66d4f36be86f3d5a3911b2e935e0d725794de2b3
komeratarun123 <komeratarun333@gmail.com>
2022-01-05 15:25:24 +0530

    First commit

---
+++ fibonacci.js
@@ -0,0 +1,16 @@
+function fibo(n) {
+    var num1 = 0;
+    var num2 = 1;
+    for (var i = 1; i <= n; i++) {
+        console.log(num1);
+        var num3 = num1 + num2;
+        //swap
+        num1 = num2;
+        num2 = num3;
+    }
+}
+fibo(10);
+
+
+
+
---
+++ fibonacci.ts
@@ -0,0 +1,17 @@
+function fibo(n:number){
+    var num1:number = 0;
+    var num2:number = 1;
+    
+    for(var i=1;i<=n;i++){
+        console.log(num1);
+        var num3:number = num1 + num2;
+
+        //swap
+        num1 = num2;
+        num2 = num3;
+    }
+}
+
+fibo(10);
+
+
